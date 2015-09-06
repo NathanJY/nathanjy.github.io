a7d5a1c271a655e626ebf807714a39e0036cbcb1
Aleksi Tappura <atappura@gmail.com>
2015-09-06 14:30:17 +0300

    Add callbacks

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
 
-	var form = ".email-form";
+	var form = '.email-form';
 	var $form = $(form);
 
+	// Form submission
 	$form.submit(function(event) {
+
+		// Act on form submission
 		$.ajax({
-		    url: "//formspree.io/hello@nathanyat.es", 
-		    method: "POST",
+		    url: '//formspree.io/hello@nathanyat.es', 
+		    method: 'POST',
 		    data: $form.serialize(),
-		    dataType: "json"
+		    dataType: 'json',
+		    success: function(data) {
+               alert(data);
+           	},
+           	error: function(data) {
+               alert(data);
+           	}
 		});
 	});
+
+	return false;
 });
